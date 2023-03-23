b62192841b3fa28f604056387b14b5c0121b659c
IkerDF <ikerdiazfalcon@alumno.ieselrincon.es>
2023-03-23 11:19:57 +0000

    a

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-const body = document.querySelector("body");
-const toggle = document.querySelector("#toggle");
-const sunIcon = document.querySelector(".toggle .bxs-sun");
-const moonIcon = document.querySelector(".toggle .bx-moon");
+function cambiarColor() {
+    const navbar = document.getElementById("navbar");
+    const color = document.querySelector(".light");
 
-toggle.addEventListener("change", () => {
+    if (document.body.className === "light") {
+        navbar.classList.remove("bg-light");
+        navbar.classList.remove("navbar-light");
+        navbar.classList.add("navbar-dark");
+        navbar.classList.add("bg-dark");
     
-    body.classList.toggle("dark");
-    sunIcon.className = sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
-    moonIcon.className = moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
-
-});
+        document.body.className = "dark";
+    } else {
+        navbar.classList.remove("bg-dark");
+        navbar.classList.remove("navbar-dark");
+        navbar.classList.add("navbar-light");
+        navbar.classList.add("bg-light");
+    
+        document.body.className = "light";
+    }
+}
